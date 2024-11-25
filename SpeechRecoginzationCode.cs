443c4d6e7d3e662f001ee091df6370aa49c10993
kudali21 <164449032+kudali21@users.noreply.github.com>
2024-11-25 23:33:07 +0530

    Add files via upload

---
+++ SpeechRecoginzationCode.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer Joe = new SpeechSynthesizer();
+        SpeechRecognitionEngine startlistening = new SpeechRecognitionEngine();
+        Random rnd = new Random();
+        int RecTimeOut = 0;
+        DateTime TimeNow = DateTime.Now;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startlistening.SetInputToDefaultAudioDevice();
+            startlistening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startlistening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startlistening_SpeechRecognized);
+        
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int ranNum;
+            string speech = e.Result.Text;
+
+            if (speech == "Hello")
+            {
+                Joe.SpeakAsync("Hello, I am here");
+            }
+
+            if (speech == "How are you")
+            {
+                Joe.SpeakAsync("I am doing good");
+            }
+
+            //if (speech == "Do you know BTS")
+            //{
+            //    Joe.SpeakAsync("Ofcourse i am a big fan");
+            //}
+
+
+            //if (speech == "Do you know jungkook")
+            //{
+            //    Joe.SpeakAsync("Oh yes, I love him");
+            //}
+
+            if (speech == "What time is it")
+            {
+                Joe.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Stop talking")
+            {
+                Joe.SpeakAsyncCancelAll();
+                ranNum = rnd.Next(1, 2);
+                if (ranNum == 1)
+                {
+                    Joe.SpeakAsync("Yes Mam");
+                }
+               
+                if (ranNum == 2)
+                {
+                    Joe.SpeakAsync("I am sorry, I will be quiet");
+                }
+   
+            }
+
+            if (speech == "Stop listening")
+            {
+                Joe.SpeakAsync("If you need me just ask");
+                _recognizer.RecognizeAsyncCancel();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+
+            if (speech == "Show commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefaultCommands.txt"));
+                LstCommamds.Items.Clear();
+                LstCommamds.SelectionMode = SelectionMode.None;
+                LstCommamds.Visible = true;
+                foreach (string command in commands)
+                {
+                    LstCommamds.Items.Add(command);
+                }
+            }
+
+            if (speech == "Hide commands")
+            {
+                LstCommamds.Visible = false;
+            }
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startlistening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            if (speech == "Wake up")
+            {
+                startlistening.RecognizeAsyncCancel();
+                Joe.SpeakAsync("Yes, I am here");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        } 
+        
+
+        private void TmrSpeaking_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if ( RecTimeOut == 11) 
+            {
+                TmrSpeaking.Stop();
+                startlistening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+    }
+}
